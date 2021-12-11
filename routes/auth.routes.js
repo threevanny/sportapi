e4323b161d50e4e5f48d036a2b19310154c84b46
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2021-12-11 12:31:02 -0500

    fixing cors

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const { auth } = require('../middlewares/auth')
 const router = express.Router()
+var cors = require('cors')
+
+var corsOptions = {
+  origin: 'https://threevanny.github.io/sportapp',
+  optionsSuccessStatus: 200 
+}
 
 const {
   register,
@@ -10,8 +16,8 @@ const {
   getUser,
 } = require('../controllers/auth.controller')
 
-router.post('/api/register', register)
-router.post('/api/login', login)
+router.post('/api/register', cors(corsOptions), register)
+router.post('/api/login', cors(corsOptions), login)
 router.post('/api/profile', auth, goToProfile)
 router.get('/api/logout', auth, logout)
 router.post('/api/user/:id', getUser)
